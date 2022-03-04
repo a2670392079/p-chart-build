d6322cb96590ef67ce78729e6a57faac536dd2c2
a2670392079 <37444340+a2670392079@users.noreply.github.com>
2022-03-04 21:01:36 +0800

    feat(components): create svg base components

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import "./global.css";
 import SVGContainer from "./components/visiual/SVGContainer";
 import FormContainer from "./components/normal/FormContainer";
 import ReactDOM from "react-dom";
-import { appendSVG, SVGConfig} from "./utils/SVGUtils";
+import { appendSVG, SVGConfig } from "./utils/SVGUtils";
 
 const MainApp: React.FC = () => {
-  const svgContainerRef = useRef();
-
-  const addSVG = (config: SVGConfig) => {
-    appendSVG(svgContainerRef.current, config);
+  const svgContainerRef = useRef<HTMLDivElement>(null);
+  const addSVG = () => {
+    return svgContainerRef.current
   };
   return (
     <div className=" flex flex-row min-h-screen h-screen w-screen bg-gray-50">
